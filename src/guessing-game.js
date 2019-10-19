2526d6ae0120e22d5d6590eecd1324862390d744
bakhodirradjabov <bakhodir.radjabov@olsoft.biz>
2019-10-19 20:26:01 +0500

    init

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.min = 0
+        this.max = 0
+        this.gessedNum = 0
+    }
+
 
     setRange(min, max) {
-
+        //console.log(min)
+        //console.log(max)
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
 
+        this.gessedNum = Math.ceil((this.min + this.max) / 2);
+        //console.log(this.gessedNum)
+        return this.gessedNum;
     }
 
     lower() {
-
+        //this.min = min;
+        this.max = this.gessedNum;
     }
 
     greater() {
-
+        this.min = this.gessedNum;
     }
 }
 
